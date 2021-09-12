6dd8880dc5a695231aa5d851269bfd27c80e77c7
andris96 <andreas-i-2010@hotmail.com>
2021-09-12 15:41:48 +0200

    Setting up everything for project 1
    posting everything that is relevant, managed to not add the files in previous commit

---
+++ Project_1/h.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void function()
+{
+    std::cout << "time to do something\n";
+}
---
+++ Project_1/main.cpp
@@ -0,0 +1,9 @@
+// Just setting up preparation for project 1
+// this might look very different when im done
+
+#include "h.hpp"
+
+int main()
+{
+    function();
+}
