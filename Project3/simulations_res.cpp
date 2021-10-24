feff812ed9969692f83bade1d8b995b83ece9f36
rajithara <rajitha@student.matnat.uio.no>
2021-10-24 14:48:34 +0200

    framework for problem 10, task 1

---
+++ Project3/simulations_res.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+
+#include "Particle.hpp"
+#include "PenningTrap.hpp"
+
+int main() {
+
+    // Setting parameters for the simulation
+    //double k_e = 1.38935333*10e5; // Coulomb constant, [u*(\mu*m)^2 / e*(\mu*s)^2]
+    double T = 9.64852558*10e1; // Tesla, [u / e*(\mu*s)]
+    double V = 9.64852558*10e7; // Volt, [u*(\mu*m)^2 / e*(\mu*s)^2]
+
+    int q_ca = 1;
+    double m_ca = 40.08; // [u]
+
+    double tmax = 500; //500 micro seconds
+    int t_steps = 100; 
+    double dt = tmax/t_steps;
+
+    double B0 = 1*T;  // Magnetic field strength, Tesla
+    double V0 = 0.0025*V; // Electric potential, Volt
+    double d = 500; // 500 micrometers = 0.05 cm
+
+    // Instantiating trap with particle interactions disabled
+    PenningTrap trap = PenningTrap(B0, V0, d);
+    trap.disable_particle_interaction();
+
+    // Initiating with 100 randomly initialized Ca-particles. For each of
+    // the amplitudes f = {0.1, 0.4, 0.7}, the fraction of particles still 
+    // inside the trap after 500 microseconds is recorded as a function 
+    // of the angular frequency \omgega_V \in [0.2, 2.5] MHz. 
+    // The results are exported to .txt files for later plotting in 
+    // python.
+    //--------------------------------------------------------------
+    std::vector<double> f_vec{0.1, 0.4, 0.7};
+    
+    double omega_start = 0.2;
+    double omega_end = 2.5;
+    double dOmega = 0.02;
+    int omega_steps = int((omega_end - omega_start) / dOmega);
+
+    int numb_particles = 100; // <---
+
+    for (int k = 0; k < f_vec.size(); k++) {
+        // For storing results, row-format: [omega_V fraction]
+        arma::mat fractions(omega_steps, 2); 
+        arma::vec result = arma::vec(2);
+
+        double f = f_vec.at(k);
+        for (int j = 0; j < omega_steps; j++) {
+            double omega_V = omega_start + j*dOmega;
+
+            trap.fill_with_particles(q_ca, m_ca, numb_particles);
+            trap.set_f(f);
+            trap.set_omega_V(omega_V);
+
+            // Evolving the system for 500 us
+            for(int i = 0; i < t_steps; i++){
+                trap.evolve_RK4(dt);
+            }
+
+            double fraction = trap.count_particles()/numb_particles;
+            result << omega_V << fraction;
+            fractions.row(j)  = result.t();
+
+            trap.reset();
+        }
+
+        fractions.save("fractions_f_" + std::to_string(f).substr(0, 4) + ".txt", arma::raw_ascii);
+    }
+
+    return 0;
+}
---
+++ Project3/simulations_res.py
@@ -0,0 +1,22 @@
+import re
+import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#foldername = "outputs"
+filenames_list = glob.glob("*.txt")
+
+fig, ax = plt.subplots(1, 1)
+for filename in filenames_list:
+    if "fractions" in filename:
+        f = filename[-8:-4] # Extracting f value
+        data = np.loadtxt(filename)
+        omega_V, fraction = data[:, 0], data[:, 1]
+        ax.plot(omega_V, fraction, label=f"f={f}")
+        ax.legend()
+
+plt.xlabel(r"$\omega_V$ [MHz]")
+plt.ylabel("fraction")
+fig.savefig("fractions_plots.pdf")
+plt.close()
