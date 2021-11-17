ace6522d25f86d398366d05b892c736bf3bc03ff
rajithara <rajitha@student.matnat.uio.no>
2021-11-17 15:12:52 +0100

    included some print statements for comparing anal and num; not satisfactory..

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -14,13 +14,20 @@ int main() {
     // Set params
     double T = 1.0;
     int L = 2;
+    double beta = 1./T;
 
-    int max_trials = 100;
-    int max_cycles = 20;
+    int max_trials = 10000;
+    int max_cycles = 500;
 
     // Instantiate
-    IsingModel L2 = IsingModel(L, T);
+    IsingModel L2(L, T);
+    TestIsingModel analytical;
+
     L2.estimate_quantites_with_MCMC(max_cycles, max_trials);
+
+    // Analytical for 2x2
+    std::cout << "Cv analytical: " << analytical.Cv(beta, T,  max_trials) << std::endl;
+    std::cout << "X analytical: " << analytical.khi(beta, max_trials) << std::endl;
 
     return 0;
 }
