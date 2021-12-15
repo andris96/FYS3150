1825ef35d2cf783d7cbee952f5bd83e2c8ee92ee
rajithara <rajitha@student.matnat.uio.no>
2021-12-15 02:53:17 +0100

    plotting okay for problem 7,8 and 9, but .bin files for single and triple slit simulations missing in order to make all plots in problem 9

--- Project5/plot.py
+++ Project5/plot.py
@@ -3,8 +3,34 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+def heatmap2D(z, xmin, xmax, ymin, ymax, filename, xlabel, ylabel, cbar_label, cmap):
+    """Produce heatmap plot of $z and save as $filename .pdf
+    
+    Partly adapted from:
+    https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+    
+    params:
+    -------
+    z (np.array) : 2D array of shape (N, N)
+    xmin, xmax, ymin, ymax (float) : Setting boundaries for X- and Y-axis
+    filename (string) : Name of the file the figure should be saved to (without extension)
+    xlabel (string) : Label of x-axis
+    ylabel (string) : Label of y-axis
+    cbar_label (string) : Label of the color bar or z-axis
+    cmap (string) : Matplotlib color map
+    """
+    ax = plt.gca()
 
+    im = ax.imshow(z, cmap=cmap, vmin=z.min(), vmax=z.max(), extent=(xmin, xmax, ymin, ymax))
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel(cbar_label, rotation=-90, va="bottom")
 
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.figure.savefig(f"{filename}.pdf")
+    plt.close()
+
+# Initializing params
 dt = 2.5E-5
 T = 0.002
 M = 201
@@ -14,27 +40,77 @@ x_points = np.arange(0, 1+h, h)
 y_points = np.arange(0, 1+h, h)
 x, y = np.meshgrid(x_points, y_points, sparse=True)
 
-p_sum = np.fromfile("p_sum.bin")
+#####################################################
+# Problem 7
 
-t = np.linspace(0,T,tsteps)
+# Plots of the deviation from of the total probablity
+# from 1.0 as a functions of time. 
 
-plt.plot(t,p_sum)
-plt.savefig("p_sum.pdf")
-
-p = np.fromfile("p.bin")
-plt.plot(t,p)
-plt.savefig("p.pdf")
-
-p_given_x = np.fromfile("p_given_x.bin")
-plt.plot(y_points,p_given_x)
-plt.savefig("p_given_x.pdf")
-
-
-U_data = np.fromfile("U_real.bin")
-
-U_data = U_data.reshape(tsteps,M,M)
+# Case 1 : no slit barrier
+# Case 2 : double-slit barrier
+#####################################################
+p_sum_cases = [np.fromfile("p_sum.bin")]
+#p_sum_cases = [np.fromfile("p_sum_case1.bin"), np.fromfile("p_sum_case2.bin")]
+t = np.linspace(0, T ,tsteps)
+for case, p_sum in enumerate(p_sum_cases):
+    plt.plot(t, p_sum)
+    plt.xlabel("Time")
+    plt.ylabel("Probability")
+    plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+    plt.title("") # Title or no title?
+    plt.savefig(f"p_sum_{case+1}.pdf")
 
 
+#####################################################
+# Problem 8
+
+# Heatmap plots that illustrate the time evolution of
+# the 2D probability function p^n, as well as for 
+# Re(U) and Im(U), for times t=0, t=0.001 and t=0.002
+#####################################################
+p = np.fromfile("p.bin").reshape(tsteps, M, M)
+U_real = np.fromfile("U_real.bin").reshape(tsteps, M, M)
+U_img = np.fromfile("U_imag.bin").reshape(tsteps, M, M)
+
+data = {"p" : p, "U_real" : U_real, "U_img" : U_img}
+
+for t_idx in [0, tsteps//2, -1]:
+    for quantity in data:
+        heatmap2D(z=data[quantity][t_idx, :, :],
+                  xmin=x.min(), xmax=x.max(), 
+                  ymin=y.min(), ymax=y.max(),
+                  filename=f"heatmap_{quantity}_{t_idx}", 
+                  xlabel="x", ylabel="y",
+                  cbar_label=f"{quantity}", cmap="viridis")
+
+#####################################################
+# Problem 9
+
+# Plot of the (normalized) probability distribution
+# at x=0.8 for time t=0.002
+#####################################################
+p_single = None #np.fromfile("p_single.bin").reshape(tsteps, M, M)
+p_triple = None #np.fromfile("p_trippel.bin").reshape(tsteps, M, M)
+ps = {"single slit" : p_single, "double slit" : p, "triple slit" : p_triple}
+
+for slit in ps:
+    if ps[slit] is not None: # remove this if-condition when p_single.bin and p_triple.bin are valid files
+        x_idx = int(0.8 / ((1 - 0) / M))
+        p_given_x = ps[slit][-1, :, x_idx] # p(y|x=0.8;t=0.002), t=0.002 eqv. to last time idx
+        p_given_x_normalized = p_given_x / np.linalg.norm(p_given_x, ord=1)
+
+        plt.plot(y, p_given_x_normalized)
+        plt.xlabel("y")
+        plt.ylabel("P", rotation=90)
+        plt.title(f"p(y|x=0.8;t=0.002), {slit}")
+        plt.savefig(f"1D_prob_dist_{slit.split()[0]}_{slit.split()[1]}.pdf")
+
+#####################################################
+# Problem X
+
+# Make animations of the double slit simulation
+#####################################################
+U_data = U_real 
 
 # Some settings
 fontsize = 12
@@ -65,9 +141,8 @@ plt.yticks(fontsize=fontsize)
 time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                     horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-
-# Function that takes care of updating the U data and other things for each frame
 def animation(i):
+    """Takes care of updating the U data and other things for each frame"""
     # Normalize the colour scale to the current frame?
     #U_max = np.max(U_data[i])
     #U_max_root = np.sqrt(U_max)
@@ -85,5 +160,4 @@ def animation(i):
 
 # Use matplotlib.animation.FuncAnimation to put it all together
 anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, len(U_data), 2), repeat=False, blit=0)
-
 anim.save('./animation.mp4', writer="ffmpeg", bitrate=-1, fps=30)
